da877c0e54b684bc1048502ea001c617485845cb
Michal Urban <michal.urban@resco.net>
2026-07-14 09:41:58 +0200

    Initial Commit

---
+++ NivyBar/NivyBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  NivyBarApp.swift
+//  NivyBar
+//
+//  Created by Michal Urban on 14/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct NivyBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
